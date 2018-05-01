f1b38e5776b4f8540a0138917161edcc222f919b
Nicolas Mohr <Nico.Mohr@gmx.net>
2018-05-01 22:20:54 +0200

    open file functionality added

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 use std::env;
+use std::fs::File;
+use std:io::prelude::*;
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("I'm searching for {}", query);
+    println!("in file {}", filename);
+
+
+    let mut f = File::open(filename).expect("Can't find the file");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("Encountered an error reading the file")
+
+    println!("With text: \n{}", contents);
 }
