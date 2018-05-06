d1b9265801bd7153e085f751793ae7fd0bc4e77a
Nicolas Mohr <Nico.Mohr@gmx.net>
2018-05-06 21:02:16 +0200

    added case insensitive mode& errors go to stderr now

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -17,7 +17,7 @@ fn main() {
     println!("in file {}", config.filename);
 
     if let Err(e) = simplegrep::run(config){
-        println!("Application Error: {}", e);
+        eprintln!("Application Error: {}", e);
         process::exit(1);   
     }
 }
